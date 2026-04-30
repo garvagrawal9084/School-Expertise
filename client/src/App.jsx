a993196d3f95a623a8a396bab19e20c1c71c6082
garvagrawal9084 <garvagrawal9084@gmail.com>
2026-04-30 22:20:15 +0530

    Tailwind added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import './index.css'
 
 function App() {
   const [data, setData] = useState()
 
-  useEffect(() => {
-    fetch("/api/message")
-    .then((res) => res.json())
-    .then((data) => setData(data.message)) ; 
-  }, [])
+  // useEffect(() => {
+  //   fetch("/api/message")
+  //   .then((res) => res.json())
+  //   .then((data) => setData(data.message)) ; 
+  // }, [])
   
 
   return (
     <>
-      <h1>Hello from School Expertise  frontend</h1>
+      <h1 className='text-red-500'>Hello from School Expertise  frontend</h1>
       <h3>{data}</h3>
     </>
   )
